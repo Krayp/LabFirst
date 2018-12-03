1958bc42be4dd9c76aa2dffec8743e21a9131ae8
Krayp <serforgel@gmail.com>
2018-12-03 10:40:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Information about  me:\n" +
+                "Name \t Andrew\n" +
+                "Age \t 21\n" +
+                "Group \t AT-161\n" +
+                "Phone \t 0962875069");
+    }
+}
